a2e616e518d94c61fae5a44839de715a629124b9
aftabdawood <aftabdawood25@gmail.com>
2023-09-03 01:21:51 +0530

    Push all code

--- js/main.js
+++ js/main.js
@@ -186,9 +186,99 @@ $( document ).ready(function() {
 });
 $(document).mouseup(function(e) 
 {
-    var search_form = $(".search-form-wrapper");
+  var enquire_form = $("#enquire-form");
+  var search_form = $(".search-form-wrapper");
     if (!search_form.is(e.target) && search_form.has(e.target).length === 0) 
     {
       $('.search-form-wrapper').removeClass('open');
     }
+  if (!enquire_form.is(e.target) && enquire_form.has(e.target).length === 0) 
+  {
+    $('#enquire-form').removeClass('open-form');
+  }
 });
+// enqure js
+$(document).ready(function(){
+  $("#enquire").click(function(){
+    $("#enquire-form").addClass("open-form");
+  });
+  $("#form-close").click(function(){
+    $("#enquire-form").removeClass("open-form");
+  });
+});
+// custom select 
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+x = document.getElementsByClassName("nextgen-select");
+l = x.length;
+// amt = document.getElementById("amount");
+// console.log(amt);
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 0; j < ll; j++) {
+    c = document.createElement("DIV");
+    if(j==0){
+      c.setAttribute("class", "same-as-selected");
+    }
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+
+        var y, i, k, s, h, sl, yl;
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+}
+function closeAllSelect(elmnt) {
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+document.addEventListener("click", closeAllSelect);
+// custom select end
+
+// Paymentjs
